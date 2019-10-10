c5ab0472ff615ef8dacac19ebd5ffa982cca7545
Thomas Winterling <info@winterling-labs.com>
2019-10-10 07:08:52 +0200

    Create type line

--- src/assets/js/function.js
+++ src/assets/js/function.js
@@ -2,7 +2,11 @@ import $ from 'jquery';
  
 $(document).ready(function(){
 
+    var myChartObject = document.getElementById('myChart');
 
+    var chart = new Chart(myChartObject,{
+        type: 'line'
+    });
 
 });
 
