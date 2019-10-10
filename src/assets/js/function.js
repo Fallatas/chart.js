c45904a7f150bf29323898b791ad09d41a68f40f
Thomas Winterling <info@winterling-labs.com>
2019-10-10 07:11:40 +0200

    Create 2 new datasets

--- src/assets/js/function.js
+++ src/assets/js/function.js
@@ -5,7 +5,21 @@ $(document).ready(function(){
     var myChartObject = document.getElementById('myChart');
 
     var chart = new Chart(myChartObject,{
-        type: 'line'
+        type: 'line',
+
+        datasets: [{
+            label: 'Dataset number 1',
+            backgroundColor: 'rgba(65,105,225,0.4)',
+            borderColor: 'rgba(65,105,225,0.4)',
+            data: [3,7,5,2,7,8,6,4 /* mydata */]
+
+        },{
+            label: 'Dataset number 2',
+            fill: true,
+            backgroundColor: 'rgba(255,0,0,0.4)',
+            borderColor: 'rgba(255,0,0,0.4)',
+            data: [5,6,2,5,6,5,5,4 /* mydata */]
+        }]
     });
 
 });
